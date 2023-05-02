3a7c6db2808e1fee6c410ddbddb3567adaa8b0cb
Juan Encabo <encabojuan@gmail.com>
2023-05-02 19:30:33 -0300

    agrego cambios al sidebar y cambios al carrusel, al carrusel le faltan correciones y optimizaciones en el codigo del slider.js

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -12,14 +12,16 @@ export class CarrouselComponent implements OnInit {
 
 	showNavigationArrows = true;
 	showNavigationIndicators = true;
-  images = ['../../../assets/images/imgl21.jpg','../../../assets/images/imgl22.jpg','../../../assets/images/imgl21.jpg'];
+  images = ['../../../assets/images/imgl21.jpg','../../../assets/images/imgl22.jpg','../../../assets/images/imgl23.jpg'];
 	constructor(config: NgbCarouselConfig, private loadScript:LoadScriptsService) {
     loadScript.load(['slider']);
 		// customize default values of carousels used by this component tree
 		config.showNavigationArrows = true;
 		config.showNavigationIndicators = false;
     config.interval = 3000;
-    
+    config.pauseOnFocus = false;
+    config.pauseOnHover = false;
+
 	}
   ngOnInit(): void {
   }
--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,22 +1,57 @@
 let sidebars = document.getElementById("sidebar");
 let carrousel = document.getElementById("carrousel");
 let changeSize = document.getElementById("changeSizeCarrousel");
-changeSize.addEventListener("click", e=>{
-    // obtengo el tamaño del sidebar cuando se clikea el boton del menu
+changeSize.addEventListener("click", changeSizeCarousel);
+window.onresize = changeWindows;
 
-    //como lo obtengo cuando lo apreto, me da el tamaño antes de que se achique
-    //es decir si el sidebar esta abierto y apreto el boton del menu y se achica me sigue dando el tamaño 260 porque me lo da antes de que se achique
-    // y si esta achicado y apreto el boton del menu se agranda y antes de que se agrande me da el tamaño que en este caso es 78
-    //teniendo esto en cuenta puedo modificar el tamaño del carrousel dependiendo si esta abierrto y cerrado,
-    //si el tamaño es igual a 260 quiere decir que apreto el boton y se achico a 78 entonces para calcular el tamaño del carousel es la diferencia de la pantalla del cliente
-    //menos el sidebar
-    let widthSidebar = sidebar.clientWidth;
-    if (widthSidebar == 260){
-        carrousel.style.width = screen.width-100 + "px";
-    }else if (widthSidebar == 78){
-        carrousel.style.width = screen.width-300 + "px";
+window.addEventListener('load', changeWindows);
+
+
+//este metodo se encarga de adaptar el tamaño del carrousel ni bien se carga la web o se cambia de ventana
+function changeWindows() {
+  let widthSidebar = sidebars.clientWidth;
+  if (screen.width > 400) {
+
+    if (widthSidebar == 260) {
+      carrousel.style.width = screen.width - 300 + "px";
+    } else {
+      carrousel.style.width = screen.width - 300 + "px";
+    }
+  } else {
+    if (widthSidebar == 260) {
+      carrousel.style.width = screen.width - 100 + "px";
+    } else if (widthSidebar == 78) {
+      carrousel.style.width = screen.width - 300 + "px";
 
     }
-    
-})
+
+  }
+
+}
+
+
+
+function changeSizeCarousel() {
+  // obtengo el tamaño del sidebar cuando se clikea el boton del menu
+
+  //como lo obtengo cuando lo apreto, me da el tamaño antes de que se achique
+  //es decir si el sidebar esta abierto y apreto el boton del menu y se achica me sigue dando el tamaño 260 porque me lo da antes de que se achique
+  // y si esta achicado y apreto el boton del menu se agranda y antes de que se agrande me da el tamaño que en este caso es 78
+  //teniendo esto en cuenta puedo modificar el tamaño del carrousel dependiendo si esta abierrto y cerrado,
+  //si el tamaño es igual a 260 quiere decir que apreto el boton y se achico a 78 entonces para calcular el tamaño del carousel es la diferencia de la pantalla del cliente
+  //menos el sidebar
+  let widthSidebar = sidebars.clientWidth;
+  if (widthSidebar == 260) {
+    carrousel.style.width = screen.width - 100 + "px";
+  } else if (widthSidebar == 78) {
+    carrousel.style.width = screen.width - 300 + "px";
+
+  }
+
+  console.log("Tamanio de carrusel " + carrousel.clientWidth);
+  console.log("tamanio pantalla " + screen.width);
+}
+
+
+
 
